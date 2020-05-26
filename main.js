718cddb7e250231781fd4c2632583d17320aea02
Maciej Rapacki <mac.rapacki@gmail.com>
2020-05-26 10:27:51 +0100

    - first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+let numberAtt = 0
+let numberToFind = ""
+
+const playerNumber = document.querySelector("input")
+const checkBtn = document.querySelector('.check')
+const attempt = document.querySelector('.attempt')
+const lastGuess = document.querySelector('.lastGuess')
+const hint = document.querySelector('.hint')
+
+
+
+checkBtn.addEventListener('click', () => {
+    const value = playerNumber.value;
+    if (value === "" || value > 100) {
+        alert('type number between 0-100')
+    } else {
+
+        if (value > numberToFind) {
+            hint.innerHTML = `Number is less than ${value}`;
+            numberAtt++;
+            attempt.innerHTML = `${numberAtt}`;
+            lastGuess.innerHTML = value;
+        } else if (value < numberToFind) {
+            hint.innerHTML = `Number is greater than ${value}`
+            numberAtt++;
+            attempt.innerHTML = `${numberAtt}`;
+            lastGuess.innerHTML = value;
+        } else {
+            numberAtt++;
+            alert('Congratulations, You guessed the number')
+
+        }
+    }
+
+})
+
+const draw = () => {
+    numberToFind = Math.floor(Math.random() * 101)
+}
+
+draw()
